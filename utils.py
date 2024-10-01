607f02bf8300d2db975537a2fc3ce2dafceecddf
gtegner <gustafte@kth.se>
2024-10-01 08:47:00 +0200

    removed freq

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 
 def generate_model_name(args):
-    return f"{args.model_name}-model-{args.model}-{args.adaptation}-context-{args.context_dim}-steps-{args.steps}-dataset-{args.dataset}-num-freq-{args.num_frequency}-num-tasks-{args.num_params}-supp-sz-{args.support_size}-noise-{args.noise_std}-traj-{args.use_trajectory}-seed-{args.seed}"
+    return f"{args.model_name}-model-{args.model}-{args.adaptation}-context-{args.context_dim}-steps-{args.steps}-dataset-{args.dataset}-num-tasks-{args.num_params}-supp-sz-{args.support_size}-noise-{args.noise_std}-traj-{args.use_trajectory}-seed-{args.seed}"
 
 def create_dir(dir_name):
     if not os.path.exists(dir_name):
